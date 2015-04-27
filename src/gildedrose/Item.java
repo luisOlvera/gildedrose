fb81e290f7479d468a3b5cec7810bc0f7f12d2c0
luisOlvera <luis.olvera@tralix.com>
2015-04-27 18:38:45 -0500

    Initial commit

---
+++ src/gildedrose/Item.java
@@ -0,0 +1,25 @@
+package gildedrose;
+
+/**
+ * Created by luisolvera on 27/04/15.
+ */
+public class Item {
+
+
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+}
